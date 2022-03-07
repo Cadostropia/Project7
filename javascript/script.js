c30ba1ac5fc4e7465f1dba8dc9561c45f34dc3f0
Salih <salihyakuphankeskin@gmail.com>
2022-03-07 08:10:50 +0300

    master

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,14 @@
 'use strict';
+document.getElementById("showScreen").innerHTML =`Waiting for Data ...`;
+
+fetch('/json/data.json')
+.then(data => console.log("First attempt "+data));
+
+fetch('/json/data.json')
+.then(recieve => recieve.json())
+.then(data => console.log("Second attempt"+data));
+
+fetch('/json/data.json')
+.then(recieve => recieve.json())
+.then(data => data[0])
+.then(data => console.log("Second attempt"+data));
