477348138708b508d693f41fc4e3d4da16a707f6
Salih <salihyakuphankeskin@gmail.com>
2022-03-07 15:41:57 +0300

    master

--- javascript/script.js
+++ javascript/script.js
@@ -1,45 +1,28 @@
 'use strict';
-// document.getElementById("showScreen").innerHTML = ``;
 
-// fetch('/json/data.json')
+// let array = {
+//     "name": `${nameD}`,
+//     "surname": `${surnameD}`,
+//     "id": `${surnameD}`,
+//     "password": `${surnameD}`
+// };
+// console.log(array);
+// fetch(fileName)
+// .then(recieve => recieve.json())
+// .then(data => data + array)
+// .then(data => console.log(data));
+
+
+// fetch(fileName)
 // .then(recieve => recieve.json())
 // .then(recieve => recieve[0])
-// .then(data => console.log("First attempt "+data));
+// .then(data => console.log(data));
 
-// fetch('/json/data.json')
-// .then(recieve => recieve.json())
-// .then(data => console.log("Second attempt "+data));
-const fileName = '/json/data.json';
+// changeText()
 
 fetch(fileName)
 .then(recieve => recieve.json())
 .then(data => SortBy(data));
-function dataColletor(){
-    let nameD, surnameD, idD, passwordD = "";
-    let array ;
-    nameD = document.getElementById("name").innerHTML;
-    surnameD = document.getElementById("name").innerHTML;
-    idD = document.getElementById("name").innerHTML;
-    passwordD = document.getElementById("name").innerHTML;
-    array = {
-        "name": `${nameD}`,
-        "surname": `${surnameD}`,
-        "id": `${surnameD}`,
-        "password": `${surnameD}`
-    };
-
-}
-
-function submitData(variable){
-    dataColletor();    
-    fetch(fileName, 
-        {
-        method: 'post',
-        body: JSON.stringify(array)})
-    .then(response => response.text());
-
-}
-// changeText()
 function SortBy(data){
     document.getElementById("showScreen").innerHTML=``;
     for (let i = 0; i< data.length; i++) {
